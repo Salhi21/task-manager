925ae1b998b4ac4719e143f345f594696539a8cd
Salhi Nassim <salhi.nassim.21@gmail.com>
2022-06-04 22:30:01 +0100

    third commit

--- api/app.js
+++ api/app.js
@@ -1,30 +1,76 @@
 const express = require('express');
 const app = express();
-// ROUTE HANDLERS
 
-// LIST ROUTES
+const { mongoose } = require('./db/mongoose');
 
-/*
-* GET /lists
-* Purpose : Get all lists
+const bodyParser = require('body-parser');
 
-*/
+// Load in the mongoose models
+const { List, Task } = require('./db/models');
+
+
+// Load middleware
+app.use(bodyParser.json());
+/* ROUTE HANDLERS */
+
+/* LIST ROUTES */
+
+/**
+ * GET /lists
+ * Purpose: Get all lists
+ */
 app.get('/lists', (req, res) => {
-    // We want to get an array of all lists in the database
+    // We want to return an array of all the lists that belong to the authenticated user 
+    List.find().then((lists) => {
+        res.send(lists);
+    }).catch((e) => {
+        res.send(e);
+    });
 })
 
-/*
- * Post /lists
- * Purpose : Create a list
+/**
+ * POST /lists
+ * Purpose: Create a list
  */
 app.post('/lists', (req, res) => {
     // We want to create a new list and return the new list document back to the user (which includes the id)
-    //The list information(fields) will be passed in via the JSON request body
-})
+    // The list information (fields) will be passed in via the JSON request body
+    let title = req.body.title;
 
+    let newList = new List({
+        title,
+    });
+    newList.save().then((listDoc) => {
+        // the full list document is returned (incl. id)
+        res.send(listDoc);
+    })
+});
+
+/**
+ * PATCH /lists/:id
+ * Purpose: Update a specified list
+ */
 app.patch('/lists/:id', (req, res) => {
-    // We want to update the specified list (list document with id in the URL ) with the new values specified in the JSON 
-})
+    // We want to update the specified list (list document with id in the URL) with the new values specified in the JSON body of the request
+    List.findOneAndUpdate({ _id: req.params.id }, {
+        $set: req.body
+    }).then(() => {
+        res.send({ 'message': 'updated successfully' });
+    });
+});
+
+/**
+ * DELETE /lists/:id
+ * Purpose: Delete a list
+ */
+app.delete('/lists/:id', (req, res) => {
+    // We want to delete the specified list (document with id in the URL)
+    List.findOneAndRemove({
+        _id: req.params.id,
+    }).then((removedListDoc) => {
+        res.send(removedListDoc);
+    })
+});
 app.listen(3000, () => {
     console.log("Server is listening on port 3000");
 })
