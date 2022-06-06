eb252b6a302c1e366efacb0a4d036ad77c1f2f29
Salhi Nassim <salhi.nassim.21@gmail.com>
2022-06-06 01:37:22 +0100

    added web request service

---
+++ frontend/src/app/web-request.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WebRequestService {
+   readonly ROOT_URL ;
+  constructor(private http : HttpClient ) {
+    this.ROOT_URL = 'http://localhost:3000'
+   }
+   get(uri : string){
+    return this.http.get(`${this.ROOT_URL}/${uri}`);
+   }
+
+   post(uri : string, payload : Object){
+    return this.http.get(`${this.ROOT_URL}/${uri}`,payload);
+   }
+   patch(uri : string){
+    return this.http.get(`${this.ROOT_URL}/${uri}`);
+   }
+   delete(uri : string){
+    return this.http.get(`${this.ROOT_URL}/${uri}`);
+   }
+
+}
