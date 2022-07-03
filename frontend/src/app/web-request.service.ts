ce4a3aa5a16c72276813b5a227ce7c7a8dd73462
Salhi Nassim <salhi.nassim.21@gmail.com>
2022-07-03 21:22:53 +0100

    edited root url

--- frontend/src/app/web-request.service.ts
+++ frontend/src/app/web-request.service.ts
@@ -8,7 +8,7 @@ export class WebRequestService {
   readonly ROOT_URL;
 
   constructor(private http: HttpClient) {
-    this.ROOT_URL = 'http://localhost:3000';
+    this.ROOT_URL = 'https://git.heroku.com/task-manager-21.git';
   }
 
   get(uri: string) {
