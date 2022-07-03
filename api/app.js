250e8d7770aa92705d917e68b5db5e774e47906c
Salhi Nassim <salhi.nassim.21@gmail.com>
2022-07-03 22:01:13 +0100

    edited port

--- api/app.js
+++ api/app.js
@@ -383,8 +383,8 @@ let deleteTasksFromList = (_listId) => {
 }
 
 
+const PORT = process.env.PORT || 3000
 
-
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log("Server is listening on port 3000");
 })
